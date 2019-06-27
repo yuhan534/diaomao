026a5e1a8905a442486d7188adb7b05e8d9e3770
yuhan <465631269@qq.com>
2019-06-27 21:18:28 +0800

    添加test工程至git

---
+++ test01.java
@@ -0,0 +1,9 @@
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
